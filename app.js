df8c0554d637efcc91c6b68b1e68ab7055061d3d
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-05-11 01:42:53 +0530

    fbauth

--- app.js
+++ app.js
@@ -10,24 +10,44 @@
  var session = require('express-session');
  var config = require('./config/config.js');
  var connectMongo = require('connect-mongo')(session);
+ var mongoose = require('mongoose').connect(config.dbURL);
 
  var env = process.env.NODE_ENV || 'development';
 
 if(env==='development'){
     console.log(config.sessionSecret);
     app.use(cookieParser());
-    app.use(session({secret:'A245E56VHUY8JBJV', saveUninitialized:true, resave:true}));
+    app.use(session({secret:config.sessionSecret, saveUninitialized:true, resave:true}));
 
 }else{
     app.use(cookieParser());
     app.use(session({
-        secret:'A245E56VHUY8JBJV',
+        secret:config.sessionSecret,
         store: new connectMongo({
-            url:config.dbURL,
+            // url:config.dbURL,
+            mongooseConnection: mongoose.connection,
             stringify: true
         })
     }));
 }
+
+// var userSchema = mongoose.Schema({
+//  username: String,
+//     password: String,
+//     fullname: String
+// });
+//
+// var Person = mongoose.model('users',userSchema);
+// var John = new Person({
+//  username: 'johndoe',
+//     password: 'john123',
+//     fullname: 'John Doe'
+// });
+
+// John.save(function (err) {
+//     console.log('Done!!')
+// });
+
 app.set('views',path.join(__dirname, 'views'));
  app.engine('html', hogan);
  app.set('view engine','html');
